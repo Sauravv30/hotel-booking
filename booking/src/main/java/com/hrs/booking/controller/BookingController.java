cc9d8315669d8dd8ee0a1e698be311f69c6f17f4
Saurav <sauravv30@gmail.com>
2024-01-22 20:15:15 +0530

    Comments added

--- booking/src/main/java/com/hrs/booking/controller/BookingController.java
+++ booking/src/main/java/com/hrs/booking/controller/BookingController.java
@@ -20,28 +20,60 @@ import java.util.List;
 public class BookingController implements com.hrs.booking.api.BookingApi {
     private BookingService bookingService;
 
+    /**
+     * Cancel hotel booking response entity.
+     *
+     * @param bookingId the booking id
+     * @return the response entity
+     */
     @Override
     public ResponseEntity<Delete> cancelHotelBooking(Long bookingId) {
         bookingService.cancelBooking(bookingId);
         return new ResponseEntity<>(new Delete().id(bookingId).message("Booking Cancelled"), HttpStatus.OK);
     }
 
+    /**
+     * Create user booking response entity.
+     *
+     * @param userId  the user id
+     * @param booking the booking
+     * @return the response entity
+     */
     @Override
     public ResponseEntity<List<Booking>> createUserBooking(Long userId, List<Booking> booking) {
         return new ResponseEntity<>(bookingService.createBooking(userId, booking), HttpStatus.CREATED);
     }
 
+    /**
+     * Gets booking by id.
+     *
+     * @param bookingId the booking id
+     * @return the booking by id
+     */
     @Override
     public ResponseEntity<Booking> getBookingById(Long bookingId) {
         return new ResponseEntity<>(bookingService.getBookingById(bookingId), HttpStatus.OK);
     }
 
+    /**
+     * Gets booking by user id.
+     *
+     * @param userId the user id
+     * @return the booking by user id
+     */
     @Override
     public ResponseEntity<List<Booking>> getBookingByUserId(Long userId) {
 
         return new ResponseEntity<>(bookingService.getBookingByUserId(userId), HttpStatus.OK);
     }
 
+    /**
+     * Update booking details response entity.
+     *
+     * @param bookingId the booking id
+     * @param booking   the booking
+     * @return the response entity
+     */
     @Override
     public ResponseEntity<Booking> updateBookingDetails(Long bookingId, Booking booking) {
         return new ResponseEntity<>(bookingService.updateBooking(bookingId, booking), HttpStatus.OK);
